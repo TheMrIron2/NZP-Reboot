add4be724b0cfa23a89543885592550ed6070c83
Ryan Baldwin <ryan.baldwin2011@gmail.com>
2017-12-12 21:22:25 -0500

    soft reset start

--- source/psp/engine/cmd.c
+++ source/psp/engine/cmd.c
@@ -813,7 +813,7 @@ void	Cmd_ExecuteString (char *text, cmd_source_t src)
 	
 // check cvars
 	if (!Cvar_Command ())
-		Con_Printf ("Unknown command \"%s\"\n", Cmd_Argv(0));
+		Con_Printf ("Command \"%s\" not found, check QC...\n", Cmd_Argv(0));
 	
 }
 
