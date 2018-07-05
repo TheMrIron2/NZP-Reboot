d27b2957c7d3b156934d5ea4004d5cd5b2e35dcb
Ryan Baldwin <ryan.baldwin2011@gmail.com>
2018-07-04 21:15:46 -0400

    Vita: Started branch, code structure, and hlbsp implementation

---
+++ source/vita/README.txt
@@ -0,0 +1,3 @@
+Absolutely all credit is going toward vitaQuake developer :)
+
+								- nai 6/21/2018
---
+++ source/vita/source/wad.c
@@ -0,0 +1,265 @@
+/*
+Copyright (C) 1996-1997 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+// wad.c
+
+#include "quakedef.h"
+
+int			wad_numlumps;
+lumpinfo_t	*wad_lumps;
+byte		*wad_base;
+
+void SwapPic (qpic_t *pic);
+
+/*
+==================
+W_CleanupName
+
+Lowercases name and pads with spaces and a terminating 0 to the length of
+lumpinfo_t->name.
+Used so lumpname lookups can proceed rapidly by comparing 4 chars at a time
+Space padding is so names can be printed nicely in tables.
+Can safely be performed in place.
+==================
+*/
+void W_CleanupName (char *in, char *out)
+{
+	int		i;
+	int		c;
+	
+	for (i=0 ; i<16 ; i++ )
+	{
+		c = in[i];
+		if (!c)
+			break;
+			
+		if (c >= 'A' && c <= 'Z')
+			c += ('a' - 'A');
+		out[i] = c;
+	}
+	
+	for ( ; i< 16 ; i++ )
+		out[i] = 0;
+}
+
+
+
+/*
+====================
+W_LoadWadFile
+====================
+*/
+void W_LoadWadFile (char *filename)
+{
+	lumpinfo_t		*lump_p;
+	wadinfo_t		*header;
+	unsigned		i;
+	int				infotableofs;
+	
+	wad_base = COM_LoadHunkFile (filename);
+	if (!wad_base)
+		Sys_Error ("W_LoadWadFile: couldn't load %s", filename);
+
+	header = (wadinfo_t *)wad_base;
+	
+	if (header->identification[0] != 'W'
+	|| header->identification[1] != 'A'
+	|| header->identification[2] != 'D'
+	|| header->identification[3] != '2')
+		Sys_Error ("Wad file %s doesn't have WAD2 id\n",filename);
+		
+	wad_numlumps = LittleLong(header->numlumps);
+	infotableofs = LittleLong(header->infotableofs);
+	wad_lumps = (lumpinfo_t *)(wad_base + infotableofs);
+	
+	for (i=0, lump_p = wad_lumps ; i<wad_numlumps ; i++,lump_p++)
+	{
+		lump_p->filepos = LittleLong(lump_p->filepos);
+		lump_p->size = LittleLong(lump_p->size);
+		W_CleanupName (lump_p->name, lump_p->name);
+		if (lump_p->type == TYP_QPIC)
+			SwapPic ( (qpic_t *)(wad_base + lump_p->filepos));
+	}
+}
+
+
+/*
+=============
+W_GetLumpinfo
+=============
+*/
+lumpinfo_t	*W_GetLumpinfo (char *name)
+{
+	int		i;
+	lumpinfo_t	*lump_p;
+	char	clean[16];
+	
+	W_CleanupName (name, clean);
+	
+	for (lump_p=wad_lumps, i=0 ; i<wad_numlumps ; i++,lump_p++)
+	{
+		if (!strcmp(clean, lump_p->name))
+			return lump_p;
+	}
+	
+	Sys_Error ("W_GetLumpinfo: %s not found", name);
+	return NULL;
+}
+
+void *W_GetLumpName (char *name)
+{
+	lumpinfo_t	*lump;
+	
+	lump = W_GetLumpinfo (name);
+	
+	return (void *)(wad_base + lump->filepos);
+}
+
+void *W_GetLumpNum (int num)
+{
+	lumpinfo_t	*lump;
+	
+	if (num < 0 || num > wad_numlumps)
+		Sys_Error ("W_GetLumpNum: bad number: %i", num);
+		
+	lump = wad_lumps + num;
+	
+	return (void *)(wad_base + lump->filepos);
+}
+
+/*
+=============================================================================
+
+automatic byte swapping
+
+=============================================================================
+*/
+
+void SwapPic (qpic_t *pic)
+{
+	pic->width = LittleLong(pic->width);
+	pic->height = LittleLong(pic->height);	
+}
+
+/*
+===============
+BestColor
+===============
+*/
+byte BestColor (int r, int g, int b, int start, int stop)
+{
+	int	i;
+	int	dr, dg, db;
+	int	bestdistortion, distortion;
+	int	berstcolor;
+	byte	*pal;
+
+//
+// let any color go to 0 as a last resort
+//
+	bestdistortion = 256*256*4;
+	berstcolor = 0;
+
+	pal = host_basepal + start*3;
+	for (i=start ; i<= stop ; i++)
+	{
+		dr = r - (int)pal[0];
+		dg = g - (int)pal[1];
+		db = b - (int)pal[2];
+		pal += 3;
+		distortion = dr*dr + dg*dg + db*db;
+		if (distortion < bestdistortion)
+		{
+			if (!distortion)
+				return i;		// perfect match
+
+			bestdistortion = distortion;
+			berstcolor = i;
+		}
+	}
+
+	return berstcolor;
+}
+
+byte *W_ConvertWAD3Texture(miptex_t *tex)
+{
+	byte	*in, *data, *out, *pal;
+	int		d, p;
+
+	in		= (byte *)((int) tex + tex->offsets[0]);
+	data	= out = (byte *)malloc(tex->width * tex->height * 4);
+
+	if (!data)
+		return NULL;
+
+	int image_width		= tex->width;
+	int image_height	= tex->height;
+	pal				= in + (((image_width * image_height) * 85) >> 6);
+	pal				+= 2;
+
+	for (d = 0;d < image_width * image_height;d++)
+	{
+		p = *in++;
+		if (tex->name[0] == '{' && p == 255)
+			out[0] = out[1] = out[2] = out[3] = 0;
+		else
+		{
+			p *= 3;
+			out[0] = pal[p];
+			out[1] = pal[p+1];
+			out[2] = pal[p+2];
+			out[3] = 255;
+		}
+		out += 4;
+	}
+	return data;
+}
+
+extern byte *host_otherpal;
+extern int host_fullbrights;
+
+int FindOurColor(miptex_t *tex)
+{
+	byte	*in, *data, *out, *pal;
+	int		d, p, color;
+
+	in		= (byte *)((int) tex + tex->offsets[0]);
+	data	= out = (byte *)malloc(tex->width * tex->height * 4);
+
+	if (!data)
+		return 0;
+
+	int image_width		= tex->width;
+	int image_height	= tex->height;
+	pal				= host_otherpal;
+
+	for (d = 0;d < image_width * image_height;d++)
+	{
+		p = *in++;
+		{
+			if (p > host_fullbrights){
+				color = p;	// we got a fullbright so we can sue it.
+				return color;
+			} else
+			color = p;
+		}
+	}
+
+	return color;
+}
